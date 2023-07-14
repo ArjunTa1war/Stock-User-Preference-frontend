aff6c73161869f3d055035e08d268578e86058dc
Arjun Talwar <arjuntalwar1110@gmail.com>
2023-07-15 01:06:32 +0530

    new change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Information from './components/information'
+import Login from './components/Login';
+import Preferences from './preferences';
+import suprsend from "@suprsend/web-sdk";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Register from './components/Register';
+suprsend.init(process.env.REACT_APP_WKEY,process.env.REACT_APP_WSECRET);
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+      <Route exact path="/" element = {<Information/>} />
+      <Route exact path="/login" element = {<Login/>} />
+      <Route exact path="/setting" element = {<Preferences />} />
+      <Route exact path="/register" element = {<Register />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/information.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Stockdata from './stockdata'
+import Navbar from "./Navbar"
+import suprsend from "@suprsend/web-sdk";
+
+
+export default function Information() {
+  const [symb,setSymb] = React.useState("IBM")
+  const A_API = process.env.REACT_APP_ALPHA
+  const [stockArray,setStock] =  React.useState([]);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${symb}&apikey=${A_API}`;
+        let data = await fetch(url);
+        let parseddata = await data.json();
+        if (parseddata) {
+          const timeSeriesDaily = [];
+            for (const date in parseddata["Time Series (Daily)"]) {
+              const values = parseddata["Time Series (Daily)"][date];
+              values["date"] = date;
+              timeSeriesDaily.push(values);
+            }
+            setStock(timeSeriesDaily);
+        } else {
+          console.log("API response is null or undefined");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+
+  
+  const changeSymb = async(event)=>{
+    setSymb(event.target.name);
+    try {
+      let url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${event.target.name}&apikey=${{A_API}}`;
+      let data = await fetch(url);
+      let parseddata = await data.json();
+      if (parseddata) {
+        const timeSeriesDaily = [];
+          for (const date in parseddata["Time Series (Daily)"]) {
+            const values = parseddata["Time Series (Daily)"][date];
+            values["date"] = date;
+            timeSeriesDaily.push(values);
+          }
+          setStock(timeSeriesDaily);
+      } else {
+        console.log("API response is null or undefined");
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+  
+  const sendNotification = async(event)=>{
+    suprsend.track("STOCK_PREF", {name : "check"});
+
+  }
+
+  return (
+    <div>
+      <Navbar/>
+      <h2 className='text-center my-5'>Stock Price variations of {symb}</h2>
+      <hr className='hr'></hr>
+      <div className="container">
+        <div className="row">
+          <div className="col text-center">
+            <h4>Last Updated on : {new Date().toLocaleDateString()}</h4>
+          </div>
+          <div className="col text-center">
+          <div className="dropdown">
+              <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                {symb}
+              </button>
+              <div className="dropdown-menu primary" aria-labelledby="dropdownMenuButton">
+                <Link className="dropdown-item" name = "IBM" onClick={changeSymb} to="/">IBM</Link>
+                <Link className="dropdown-item" name = "TSCO.LON" onClick={changeSymb} to="/">TSCO.LON</Link>
+                <Link className="dropdown-item" name = "SHOP.TRT" onClick={changeSymb} to="/">SHOP.TRT</Link>
+                <Link className="dropdown-item" name = "GPV.TRV" onClick={changeSymb} to="/">GPV.TRV</Link>
+                <Link className="dropdown-item" name = "DAI.DEX" onClick={changeSymb} to="/">DAI.DEX</Link>
+              </div>
+              {localStorage.getItem('token')&&<button className='mx-3 btn btn-primary' onClick={sendNotification}>Send Notification</button>}
+            </div>
+          </div>
+        </div>
+        </div> 
+        <hr className='hr'></hr>
+        <Stockdata Array={stockArray}/>
+    </div>
+  )
+}
